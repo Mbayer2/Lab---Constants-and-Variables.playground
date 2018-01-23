99dc6b2c37dc1900378f91214d1e6c6abf0f2126
Mbayer2 <mayer1@live.maryville.edu>
2018-01-23 18:52:12 -0500

    Changes

---
+++ Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+let friends = "100"
+print(friends)
+/*:
+ Now assume you go through and remove friends that aren't active on social media. Update your `friends` constant to a lower number than it currently is.
+ */
+let friends = "97"
+print(friends)
+/*:
+ Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friend` constant to a lower number so that the playground will compile properly.
+ */
+print("Because when you declare a constant it cannot change")
+
+//: page 1 of 10  |  [Next: App Exercise - Step Goal](@next)
